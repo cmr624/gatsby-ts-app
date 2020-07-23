5946cb29df57961c8d75115bea8792e7be0e1a09
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-07-22 22:35:05 -0400

    adding props interfaces

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import { Link } from "gatsby";
+import React from "react";
+
+interface HeaderProps {
+  siteTitle : string | any;
+}
+
+const Header = (props : HeaderProps) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {props.siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+
+export default Header;
